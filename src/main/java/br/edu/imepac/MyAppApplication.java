24528dfc7c825ff4cf8ffe7ad6e244ef7add433c
unknown <232-000828@IMEPACAD.local>
2024-09-11 22:27:46 -0300

    Application run

---
+++ src/main/java/br/edu/imepac/MyAppApplication.java
@@ -0,0 +1,11 @@
+package br.edu.imepac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyAppApplication.class, args);
+    }
+}
--- src/main/java/br/edu/imepac/domain/Aluno.java
+++ src/main/java/br/edu/imepac/domain/Aluno.java
@@ -4,4 +4,26 @@ public class Aluno {
     private String nome;
     private String matricula;
     private String email;
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+
+    }
+    public String getMatricula(){
+        return matricula;
+    }
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+
+    }
+
 }
